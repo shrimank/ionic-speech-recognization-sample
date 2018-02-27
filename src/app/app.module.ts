c1e61893d1fda9539cc3d16bcbe33c5494d662c0
shrimank <shrimankumbar@gmail.com>
2018-02-27 13:15:35 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+// import the plugin
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -24,6 +27,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    SpeechRecognition,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
